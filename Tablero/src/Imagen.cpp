8228e4184a5debfbf4710864d7b2bd43a39817e8
DanielFrenzel <d.frenzel@alumnos.upm.es>
2025-05-30 11:50:35 +0200

    Cambios memoria dinámica
    Se cambia algun error de memoria dinámica

--- Tablero/src/Imagen.cpp
+++ Tablero/src/Imagen.cpp
@@ -1,10 +1,19 @@
 #include "Imagen.h"
 
-Sprite* Imagen::nombreImagen = nullptr;
+vector<Sprite*> Imagen::sprites;
 
 Sprite* Imagen::crearImagen(const char* nom, float& x, float& y, float& tam_x, float& tam_y)
 {
-	nombreImagen = new Sprite{ nom,x,y,tam_x,tam_y };	//Creamos nuevo objeto
-	nombreImagen->draw();								//Lo dibujamos
-	return nombreImagen;								//Devuelvo el puntero
+	Sprite* nuevoSprite = new Sprite{ nom,x,y,tam_x,tam_y };	//Creamos nuevo objeto
+	sprites.push_back(nuevoSprite);
+	nuevoSprite->draw();								//Lo dibujamos
+	return nuevoSprite;								//Devuelvo el puntero
+}
+
+void Imagen::eliminarImagenes()
+{
+	for (Sprite* s : sprites) {
+		delete s; // Eliminamos cada Sprite
+	}
+	sprites.clear();  // Limpiamos el vector
 }
--- Tablero/src/Imagen.h
+++ Tablero/src/Imagen.h
@@ -1,21 +1,17 @@
 #pragma once
 
 #include"ETSIDI.h"
+#include<vector>
 
 using namespace ETSIDI;
+using std::vector;
 
 class Imagen
 {
 private:
-	static Sprite* nombreImagen;							//Creamos objeto Sprite y lo hacemos static
+	static vector<Sprite*> sprites;							/*Creamos objeto Sprite y lo hacemos static*/
 public:
 	static Sprite* crearImagen(const char* nom,				//Creamos la imagen
 		float& x, float& y, float& tam_x, float& tam_y);
-	~Imagen()												//Destruimos la imagen con delete
-	{
-		if (nombreImagen != nullptr)
-		{
-			delete nombreImagen;
-		}
-	}
+	static void eliminarImagenes();
 };
